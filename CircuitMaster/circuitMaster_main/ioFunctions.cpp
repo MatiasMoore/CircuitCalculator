da19d28fd61d6157a5bf4a3749eadf98836414fa
MatiasMoore <nikita.birukov.03@mail.ru>
2023-06-11 20:45:36 +0300

    New root element exceptions and comments in readInputFromFile

--- CircuitMaster/circuitMaster_main/ioFunctions.cpp
+++ CircuitMaster/circuitMaster_main/ioFunctions.cpp
@@ -22,45 +22,84 @@ QString complexToStr(std::complex<double> num)
 
 void readInputFromFile(QString inputPath, QMap<QString, CircuitConnection>& circuitMap)
 {
+    // Создаем QFile на основе пути
     QFile xmlFile(inputPath);
+
+    // Попытатья открыть файл
+    // Ошибка, если не удалось открыть
     if (!xmlFile.exists() || !xmlFile.open(QFile::ReadOnly | QFile::Text)) {
         throw QString("Неверно указан файл для входных данных. Возможно указанного расположения не существует или нет прав на запись.");
     }
 
-    // Setup DomDocument
+    // Создаем QDomDocument
     QDomDocument domDocument;
+
+    // Переменные для получения ошибки от QDomDoc
     QString errorMes;
     int errorLine;
+
+    // Пытаемся создать QDomDoc
+    // Ошибка, если не удалось создать QDomDoc
     if (!domDocument.setContent(&xmlFile, &errorMes, &errorLine))
     {
         throw QString("%1 at line %2").arg(errorMes, QString::number(errorLine));
     }
 
+    // Закрываем файл, по завершении работы
     xmlFile.close();
 
+    // Получаем корневой элемент QDomDoc
     QDomElement rootElement = domDocument.documentElement();
 
-    auto seqConnections = domDocument.elementsByTagName("seq");
-    auto parConnections = domDocument.elementsByTagName("par");
-    auto conns = { seqConnections, parConnections};
-    int voltageAttrCount = 0;
+    // Обработка ошибок корневого элемента
+    QString rootTag = rootElement.tagName();
+    if (rootTag != "seq" && rootTag != "par")
+        throw QString("Корневым элементом должно быть последовательное \"<seq>\" или параллельное \"<par>\" соединение.");
+
+    QString voltageStr = rootElement.attribute("voltage", "");
+    if (voltageStr.length() == 0)
+        throw QString("У корневого элемента должно быть указано напряжение.");
+
+    QString frequencyStr = rootElement.attribute("frequency", "");
+    // Значение -1 означает, что частота неизвестна
     double frequency = -1;
-    for (auto i = conns.begin(); i != conns.end(); i++)
+    if (frequencyStr.length() > 0)
     {
-        for (int j = 0; j < i->count(); j++)
+        // Пытаемся конвертировать в double
+        bool convertedOk;
+        frequency = frequencyStr.toDouble(&convertedOk);
+
+        // Ошибка, если не удалость конверертировать
+        if (!convertedOk)
+            throw QString("Неверный формат значения частоты у корневого элемента.");
+    }
+
+    // Элементы всех соединений цепи, кроме корневого
+    QDomNodeList seqConnections = rootElement.elementsByTagName("seq");
+    QDomNodeList parConnections = rootElement.elementsByTagName("par");
+    auto conns = { seqConnections, parConnections};
+
+    // Обработка ошибок связанных с указанием напряжения или частоты у других соединений
+    for (auto connGroup = conns.begin(); connGroup != conns.end(); connGroup++)
+    {
+        // Для каждого соединения
+        for (int i = 0; i < connGroup->count(); i++)
         {
-            QDomNode node = i->at(j);
-            QString freqStr = node.toElement().attribute("frequency");
-            if (freqStr.length() > 0)
-                frequency = freqStr.toDouble();
-            if (node.attributes().contains("voltage"))
-                voltageAttrCount++;
-            if (voltageAttrCount > 1)
-                throw QString("Повторное указание напряжения цепи на строке %1. Напряжение указывается лишь один раз для всей схемы").arg(QString::number(node.lineNumber()));
+            QDomElement connectionElement = connGroup->at(i).toElement();
+
+            // Ошибка, если указано напряжение
+            if (connectionElement.attribute("voltage", "").length() != 0)
+                throw QString("Неверное указание напряжения цепи на строке %1. "
+                              "Напряжение указывается только для корневого элемента схемы").arg(QString::number(connectionElement.lineNumber()));
+
+            // Ошибка, если указана частота
+            if (connectionElement.attribute("frequency", "").length() != 0)
+                throw QString("Неверное указание частоты переменного тока на строке %1. "
+                              "Частота указывается только для корневого элемента схемы").arg(QString::number(connectionElement.lineNumber()));
         }
     }
-    if (voltageAttrCount == 0)
-        throw QString("Не указано значение напряжения. Значение напряжения должно быть указано");
+
+    // Создаем дерево соединений в QMap
     CircuitConnection::connectionFromDocElement(circuitMap, rootElement, frequency);
 }
 
